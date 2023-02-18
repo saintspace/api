192410eb0399f3ee7b8f4aef087d50a9362c45ad
Jordan Lee Osier <jordan.osier@skillmaple.com>
2023-02-18 15:10:08 -0500

    convert to something for app runner

--- main.go
+++ main.go
@@ -1,46 +1,14 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// Response is the structure of the JSON response sent by the function.
-type Response struct {
-	Message string `json:"message"`
-}
-
-// HandleRequest is the main function handler.
-func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	// Process the request
-	message := "Hello, World!"
-
-	// Create the JSON response
-	response, err := json.Marshal(Response{
-		Message: message,
-	})
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       fmt.Sprintf("Failed to marshal response: %v", err),
-		}, nil
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-		Headers: map[string]string{
-			"Content-Type": "application/json",
-		},
-		Body: string(response),
-	}, nil
-}
-
 func main() {
-	lambda.Start(HandleRequest)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<h1>Welcome to App Runner</h1>")
+	})
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
 }
